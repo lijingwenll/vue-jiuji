b79e6190a899226e7d99c401ad84b85010b8810a
a734636849 <734636849@qq.com>
2018-01-29 12:37:12 +0800

    ico

--- my-vue/src/components/HelloWorld.vue
+++ my-vue/src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hello">
+  <div class="nav">
     
   </div>
 </template>
@@ -16,7 +16,7 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
 
 </style>
